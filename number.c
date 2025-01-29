8bd229ee54d412505965ecdb4f88485cc9d54568
arulkumarank <arulkumaran187.com>
2025-01-29 16:13:06 +0530

    initial push

---
+++ number.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main()
+{
+    int var_a;
+    printf("enter a number :");
+    scanf("%i",& var_a);
+    printf("the entered number is : %d",var_a);
+    return 0;
+}
---
+++ string.c
@@ -0,0 +1,8 @@
+#include<stdio.h>
+int main (){
+    char str[20];
+    printf("enter a character : ");
+    scanf("%s",&str);
+    printf("the entered character is :%s",str);
+    return 0;
+}
