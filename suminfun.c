a9aca74cd830d412d2edc1eb16ec0721cdd2309a
arulkumarank <arulkumaran187.com>
2025-02-12 17:52:04 +0530

    suminfun

---
+++ suminfun.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+int main(){
+    int a,b,c,d,sum;
+    for (int i = 0; i < 4; i++)
+    {
+        printf("Enter the number : ");
+        scanf("%d",&a);
+
+    }
+    printf("Sum is %d", sum);
+    return 0;
+}   
